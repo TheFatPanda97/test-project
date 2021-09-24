b1e443da6f5ca5ec498620c7e867d9efbdaed862
Shawn Hu <shawnhu97@gmail.com>
2021-09-23 21:24:17 -0400

    testing backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const App = () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-);
+const App = () => {
+  const [title, setTitle] = useState('hello world');
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get('http://localhost:8080');
+      setTitle(res.data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+};
 
 export default App;
